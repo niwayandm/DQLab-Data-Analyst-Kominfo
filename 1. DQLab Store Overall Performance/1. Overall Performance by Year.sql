c8f5603726cc9f9a05f718faa8fdaad57e3a7e23
Dedep <92382295+niwayandm@users.noreply.github.com>
2022-08-29 11:36:53 +0700

    Update 1. Overall Performance by Year.sql
    added output

--- 1. DQLab Store Overall Performance/1. Overall Performance by Year.sql
+++ 1. DQLab Store Overall Performance/1. Overall Performance by Year.sql
@@ -6,3 +6,13 @@ FROM dqlab_sales_store
 WHERE order_status = 'Order Finished'
 GROUP BY YEAR(order_date)
 ORDER BY years;
+
+console (output) ::
++-------+------------+-----------------+
+| years | sales      | number_of_order |
++-------+------------+-----------------+
+| 2009  | 4613872681 |            1244 |
+| 2010  | 4059100607 |            1248 |
+| 2011  | 4112036186 |            1178 |
+| 2012  | 4482983158 |            1254 |
++-------+------------+-----------------+

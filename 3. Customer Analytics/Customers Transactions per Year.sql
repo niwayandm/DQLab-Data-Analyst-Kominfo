ebd861d4904f75b7d20fdfa665ded4a450a9c99f
Dedep <92382295+niwayandm@users.noreply.github.com>
2022-08-29 13:32:33 +0700

    Update Customers Transactions per Year.sql
    added output

--- 3. Customer Analytics/Customers Transactions per Year.sql
+++ 3. Customer Analytics/Customers Transactions per Year.sql
@@ -5,3 +5,13 @@ FROM dqlab_sales_store
 WHERE order_status = 'Order Finished'
 GROUP BY years
 ORDER BY years;
+
+console (output) ::
++-------+--------------------+
+| years | number_of_customer |
++-------+--------------------+
+| 2009  |                585 |
+| 2010  |                593 |
+| 2011  |                581 |
+| 2012  |                594 |
++-------+--------------------+

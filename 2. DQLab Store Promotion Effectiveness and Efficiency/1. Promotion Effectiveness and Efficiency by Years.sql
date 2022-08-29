b2d7680ebdfde25b6312ffb0e52165324fcec8a6
Dedep <92382295+niwayandm@users.noreply.github.com>
2022-08-29 13:31:00 +0700

    Update 1. Promotion Effectiveness and Efficiency by Years.sql
    added output

--- 2. DQLab Store Promotion Effectiveness and Efficiency/1. Promotion Effectiveness and Efficiency by Years.sql
+++ 2. DQLab Store Promotion Effectiveness and Efficiency/1. Promotion Effectiveness and Efficiency by Years.sql
@@ -6,3 +6,14 @@ SELECT
 FROM dqlab_sales_store
 WHERE order_status = 'Order Finished'
 GROUP BY years ORDER by YEARS;
+
+console (output) ::
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
+| years | product_sub_category           | product_category | sales     | promotion_value | burn_rate_percentage |
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
+|  2012 | Office Machines                | Technology       | 811427140 |        46616695 |                 5.75 |
+|  2012 | Chairs & Chairmats             | Furniture        | 654168740 |        26623882 |                 4.07 |
+|  2012 | Telephones and Communication   | Technology       | 422287514 |        18800188 |                 4.45 |
+|  2012 | Tables                         | Furniture        | 388993784 |        16348689 |                  4.2 |
+|  2012 | Binders and Binder Accessories | Office Supplies  | 363879200 |        22338980 |                 6.14 |
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+

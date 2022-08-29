a9ad036ac0cc7d80b2e3767c1ed13d5431fce320
Dedep <92382295+niwayandm@users.noreply.github.com>
2022-08-29 13:35:15 +0700

    added output

--- 2. DQLab Store Promotion Effectiveness and Efficiency/2. Promotion Effectiveness and Efficiency by Product Sub Category.sql
+++ 2. DQLab Store Promotion Effectiveness and Efficiency/2. Promotion Effectiveness and Efficiency by Product Sub Category.sql
@@ -15,11 +15,24 @@ GROUP BY
 ORDER by sales DESC;
 
 console (output) ::
-+-------+--------------------+
-| years | number_of_customer |
-+-------+--------------------+
-|  2009 |                585 |
-|  2010 |                593 |
-|  2011 |                581 |
-|  2012 |                594 |
-+-------+--------------------+
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
+| years | product_sub_category           | product_category | sales     | promotion_value | burn_rate_percentage |
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
+|  2012 | Office Machines                | Technology       | 811427140 |        46616695 |                 5.75 |
+|  2012 | Chairs & Chairmats             | Furniture        | 654168740 |        26623882 |                 4.07 |
+|  2012 | Telephones and Communication   | Technology       | 422287514 |        18800188 |                 4.45 |
+|  2012 | Tables                         | Furniture        | 388993784 |        16348689 |                  4.2 |
+|  2012 | Binders and Binder Accessories | Office Supplies  | 363879200 |        22338980 |                 6.14 |
+|  2012 | Storage & Organization         | Office Supplies  | 356714140 |        18802166 |                 5.27 |
+|  2012 | Computer Peripherals           | Technology       | 308014340 |        15333293 |                 4.98 |
+|  2012 | Copiers and Fax                | Technology       | 292489800 |        14530870 |                 4.97 |
+|  2012 | Appliances                     | Office Supplies  | 266131100 |        14393300 |                 5.41 |
+|  2012 | Office Furnishings             | Furniture        | 178927480 |         8233849 |                  4.6 |
+|  2012 | Bookcases                      | Furniture        | 159984680 |        10024365 |                 6.27 |
+|  2012 | Paper                          | Office Supplies  | 126896160 |         6224694 |                 4.91 |
+|  2012 | Envelopes                      | Office Supplies  |  58629280 |         2334321 |                 3.98 |
+|  2012 | Pens & Art Supplies            | Office Supplies  |  43818480 |         2343501 |                 5.35 |
+|  2012 | Scissors, Rulers and Trimmers  | Office Supplies  |  36776400 |         2349280 |                 6.39 |
+|  2012 | Labels                         | Office Supplies  |  10007040 |          452245 |                 4.52 |
+|  2012 | Rubber Bands                   | Office Supplies  |   3837880 |          117324 |                 3.06 |
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+

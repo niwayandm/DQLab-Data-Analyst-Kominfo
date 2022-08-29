fd8b9a13a5e7fbcc8bd834edd54a305d11dce882
Dedep <92382295+niwayandm@users.noreply.github.com>
2022-08-29 11:39:20 +0700

    Update 2. Overall Performance by Product Sub Category.sql
    added output

--- 1. DQLab Store Overall Performance/2. Overall Performance by Product Sub Category.sql
+++ 1. DQLab Store Overall Performance/2. Overall Performance by Product Sub Category.sql
@@ -7,3 +7,43 @@ WHERE order_status = 'Order Finished'
  AND YEAR(order_date) > 2010
 GROUP BY years, product_sub_category
 ORDER BY years, sales DESC;
+
+console (output) ::
++-------+--------------------------------+-----------+
+| years | product_sub_category           | sales     |
++-------+--------------------------------+-----------+
+|  2011 | Chairs & Chairmats             | 622962720 |
+|  2011 | Office Machines                | 545856280 |
+|  2011 | Tables                         | 505875008 |
+|  2011 | Copiers and Fax                | 404074080 |
+|  2011 | Telephones and Communication   | 392194658 |
+|  2011 | Binders and Binder Accessories | 298023200 |
+|  2011 | Storage & Organization         | 285991820 |
+|  2011 | Appliances                     | 272630020 |
+|  2011 | Computer Peripherals           | 232677960 |
+|  2011 | Bookcases                      | 169304620 |
+|  2011 | Office Furnishings             | 160471500 |
+|  2011 | Paper                          | 111080380 |
+|  2011 | Pens & Art Supplies            |  43093800 |
+|  2011 | Envelopes                      |  36463900 |
+|  2011 | Labels                         |  15607780 |
+|  2011 | Scissors, Rulers and Trimmers  |  12638340 |
+|  2011 | Rubber Bands                   |   3090120 |
+|  2012 | Office Machines                | 811427140 |
+|  2012 | Chairs & Chairmats             | 654168740 |
+|  2012 | Telephones and Communication   | 422287514 |
+|  2012 | Tables                         | 388993784 |
+|  2012 | Binders and Binder Accessories | 363879200 |
+|  2012 | Storage & Organization         | 356714140 |
+|  2012 | Computer Peripherals           | 308014340 |
+|  2012 | Copiers and Fax                | 292489800 |
+|  2012 | Appliances                     | 266131100 |
+|  2012 | Office Furnishings             | 178927480 |
+|  2012 | Bookcases                      | 159984680 |
+|  2012 | Paper                          | 126896160 |
+|  2012 | Envelopes                      |  58629280 |
+|  2012 | Pens & Art Supplies            |  43818480 |
+|  2012 | Scissors, Rulers and Trimmers  |  36776400 |
+|  2012 | Labels                         |  10007040 |
+|  2012 | Rubber Bands                   |   3837880 |
++-------+--------------------------------+-----------+
